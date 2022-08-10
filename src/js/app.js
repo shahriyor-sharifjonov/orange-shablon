f14f6e25b509a50fe7dddcefa443496d64bc11f6
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2022-08-10 21:56:21 +0500

    initial

---
+++ src/js/app.js
@@ -0,0 +1,28 @@
+import * as functions from "./modules/functions.js";
+
+functions.isWebp();
+
+// import Swiper, { Navigation, Pagination } from 'swiper';
+
+// const swiper = new Swiper();
+
+const headerButton = document.querySelector(".header__button");
+const headerMenu = document.querySelector(".header__menu");
+let menuOpened = false;
+const menuToggle = () => {
+  menuOpened = !menuOpened;
+  headerButton.classList.toggle("open");
+  headerMenu.classList.toggle("open");
+  menuOpened ? document.body.classList.add('o-hidden') : document.body.classList.remove('o-hidden')
+};
+
+headerButton.onclick = menuToggle;
+
+window.onclick = (e) => {
+  if (
+    menuOpened &&
+    !e.composedPath().includes(headerButton) &&
+    !e.composedPath().includes(headerMenu)
+  )
+    menuToggle();
+};
